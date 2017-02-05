20e4ec72aa55c5b30f74d226a121a358d9c2a4f0
webdevelop <webdevelop@sohu.com>
2017-02-05 20:56:45 +0800

    vue-router demo更新

--- 3.App.js
+++ 3.App.js
@@ -3,16 +3,16 @@ const User = {
         <div>
             <h2>User {{ $route.params.id }}</h2>
             <p>
-                <router-link :to="{path: $route.params.id + '/profile'}">{{ $route.params.id }} profile</router-link>
-                <router-link :to="{path: $route.params.id + '/posts'}">{{ $route.params.id }} posts</router-link>
+                <router-link :to="{path: '/user/' + $route.params.id + '/profile'}">{{ $route.params.id }} profile</router-link>
+                <router-link :to="{path: '/user/' + $route.params.id + '/posts'}">{{ $route.params.id }} posts</router-link>
             </p>
             <router-view></router-view>
         </div>`
 };
 
-const UserHome = {template: '<div>Home</div>'};
-const UserProfile = {template: '<div>Profile</div>'};
-const UserPosts = {template: '<div>Posts</div>'};
+const UserHome = {template: '<div>{{$route.params.id}} Home</div>'};
+const UserProfile = {template: '<div>{{$route.params.id}} Profile</div>'};
+const UserPosts = {template: '<div>{{$route.params.id}} Posts</div>'};
 
 const router = new VueRouter({
     routes: [
